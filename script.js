a157b98928e65a8ec1b3d1396e7e3bc466872eb9
vengadesan2 <vengadsanguna24@gmail.com>
2023-11-03 15:25:44 +0530

    complete

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ console.log(compare(obj1,obj2));
 // // 2use the rest countries API URL ANA DISPALY ALL countries and polulation 
  
 var request=new XMLHttpRequest();
-request.open("GET","https://restcountries.eu/rest/v2/all",true);
+request.open("GET","https://restcountries.com/v3.1/all",true);
 request.send();
 request.onload=function(){
     var data = request.response;
     var result = JSON.parse(data);
     // console.log(result)
     for( var i=0;i<result.length;i++){
-        console.log(result[i].country )
+        console.log(result[i].flag )
     }
  
 }
@@ -35,15 +35,15 @@ request.onload=function(){
 // // use the same rest countries  and print all countriesname,and region,sub-region and properties
 
 var requests=new XMLHttpRequest();
-requests.open("GET","https://restcountries.eu/rest/v2/all",true);
+requests.open("GET","https://restcountries.com/v3.1/all",true);
 requests.send();
 requests.onload=function(){
     var data =requests.response;
     var result = JSON.parse(data);
     // console.log(result)
     for( var i=0;i<result.length;i++){
-        console.log(result[i].countryname)
-        console.log(result[i].sub-region)
+        console.log(result[i].currencies)
+        console.log(result[i].subregion)
         console.log(result[i].region)
     }
 
